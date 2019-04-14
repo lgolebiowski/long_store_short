27bb692daf375355ade17194576102edc8a016fc
Lukasz Golebiowski <lukasz.golebiowski@gmail.com>
2019-04-14 13:14:38 +1000

    inital prisma setup, create graphql yoga server

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  vars => {
+    console.log(vars,'vars')
+  }
+)
+
