a2cdc0a38253351939c66195b63617c09ad97b8c
Lukasz Golebiowski <lukasz.golebiowski@gmail.com>
2019-04-16 20:40:05 +1000

    configured prisma, add first Item, query and mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,27 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, context, info) {
+    // TODO: Check if they are logged in
+
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  },
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // },
+};
 
 module.exports = Mutation;
