0c77fb8fd522493e84e0bb47fca9e47040c6b5f9
Lukasz Golebiowski <lukasz.golebiowski@gmail.com>
2019-05-12 21:55:12 +1000

    add createUser, deployed to the new endpoint

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
   async createItem(parent, args, context, info) {
     // TODO: Check if they are logged in
@@ -22,13 +25,54 @@ const Mutation = {
     // the id won't be updated - remove it 
     delete updates.id
     // call update
-    return context.db.mutation.updateItem({
-      data: updates,
-      where: {
-        id: args.id,
-      }
-    })
+    return context.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+
+  async deleteItem(parent, args, context, info) {
+    const where = {id: args.id};
+    // the id won't be updated - remove it 
+    const item = context.db.query.item({ where }, `{ id title }`);
+    // call update
+    return context.db.mutation.deleteItem( 
+      { where },
+      info
+    );
+  },
+
+  async signUp(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    // create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // Finalllllly we return the user to the browser
+    return user;
   }
-};
+}
 
 module.exports = Mutation;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,8 +4,13 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   async items(parent, args, ctx, info) {
-    const items = await ctx.db.query.items();
+    const items = await ctx.db.query.items({
+      skip: args.skip,
+      first: args.first
+    });
+    
     return items;
   },
 };
